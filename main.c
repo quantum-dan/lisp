4b6d0083f55dbedb6c9543b09921ec8fac07c5b9
qpython1000100 <dphilippus123@gmail.com>
2016-10-16 20:43:48 -0600

    Parsing is fully operational--main contains demo

--- main.c
+++ main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <parse.h>
+#include <string.h>
+
+int test_parse(char *string);
 
 int main() {
-    char *a = "stuff and things are cool";
-    int result = 0;
-    int len = 25;
-    while (result != -1) {
-        printf("%s\n", a);
-        result = split(a, ' ', len);
-        a = a + result;
-        len = len - result;
+    char *string = (char *)malloc(100);
+    scanf("%20c", string);
+    test_parse(string);
+    return 0;
+}
+
+int test_parse(char *string) {
+    char *buffer = (char *)malloc(strlen(string));
+    int s = find_between(string, '(', ')', strlen(string), buffer);
+    if (s == -1) return -1;
+    int result = split(buffer, ' ', strlen(buffer));
+    if (buffer[0] == 'p') {
+        printf("%s\n", buffer + result);
+    } else {
+        printf("Failed!\n");
     }
     return 0;
 }
